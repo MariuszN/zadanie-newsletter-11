97854ff9b1e9beb1ebe91528ac89df12cfd15ca5
Mariusz <mnurkowski@op.pl>
2019-05-21 18:21:59 +0200

    Rozwiazanie zadania

--- src/main/java/pl/javastart/Runner.java
+++ src/main/java/pl/javastart/Runner.java
@@ -1,7 +1,26 @@
 package pl.javastart;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
 class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
         //TODO
+
+        Scanner scanner = new Scanner(System.in);
+        String className = scanner.nextLine();  // Należy przekazać pl.javastart.Tricky
+        String params = "JavaStart";
+
+        Class c = Class.forName(className);
+        Constructor ctorlist[] = c.getDeclaredConstructors();
+        Method methlist[] = c.getDeclaredMethods();
+
+        Constructor ct = ctorlist[0];
+        ct.setAccessible(true);
+        Method m = methlist[0];
+        Object o = ct.newInstance(params);
+        m.invoke(o);
     }
 }
